9a7524f0b66760d648b17c552390d2544eeea6fc
Amir Salehi <nik.amirsa@gmail.com>
2023-07-18 16:12:38 +0330

    try with html form

--- src/components/sections/Item.tsx
+++ src/components/sections/Item.tsx
@@ -7,7 +7,9 @@ interface Props {
 export default function Item({ id }: Props) {
     console.log(id)
 
-    const submitHandler = () => {
+    const submitHandler = (e: any) => {
+        e.preventDefault()
+
         console.log('submit run')
     }
 
@@ -15,7 +17,7 @@ export default function Item({ id }: Props) {
         <Card>
             <h1>{id}</h1>
 
-            <Form name={`item-${id}`} onFinish={submitHandler}>
+            {/* <Form name={`item_${id}`} onFinish={submitHandler}>
                 <Form.Item
                     name={`username`}
                     label='key name'
@@ -36,15 +38,18 @@ export default function Item({ id }: Props) {
                 >
                     Save
                 </Button>
-            </Form>
+            </Form> */}
 
-            {/* <form onSubmit={submitHandler}>
+            <form onSubmit={submitHandler} name={`item_${id}`}>
 
-                <input type="text" name="username" />
-                <input type="password" name="password" />
+                <label htmlFor={`username_${id}`}>Username</label>
+                <input type="text" name={`username_${id}`} id={`username_${id}`} />
+
+                <label htmlFor={`password_${id}`}>Password</label>
+                <input type="password" name={`password_${id}`} id={`password_${id}`} />
 
                 <button type="submit">submit</button>
-            </form> */}
+            </form>
         </Card>
     )
 }
